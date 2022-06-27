46a051ea7647f72633d72d1a21c64fb1956512dd
Gonas <sanoj.koch@web.de>
2022-06-27 20:58:00 +0200

    rename boilerplate in files

--- module/boilerplate.mjs
+++ module/boilerplate.mjs
@@ -1,12 +1,12 @@
 // Import document classes.
-import { BoilerplateActor } from "./documents/actor.mjs";
-import { BoilerplateItem } from "./documents/item.mjs";
+import { IconRPGActor } from "./documents/actor.mjs";
+import { IconRPGItem } from "./documents/item.mjs";
 // Import sheet classes.
-import { BoilerplateActorSheet } from "./sheets/actor-sheet.mjs";
-import { BoilerplateItemSheet } from "./sheets/item-sheet.mjs";
+import { IconRPGActorSheet } from "./sheets/actor-sheet.mjs";
+import { IconRPGItemSheet } from "./sheets/item-sheet.mjs";
 // Import helper/utility classes and constants.
 import { preloadHandlebarsTemplates } from "./helpers/templates.mjs";
-import { BOILERPLATE } from "./helpers/config.mjs";
+import { ICONRPG } from "./helpers/config.mjs";
 
 /* -------------------------------------------- */
 /*  Init Hook                                   */
@@ -16,14 +16,14 @@ Hooks.once('init', async function() {
 
   // Add utility classes to the global game object so that they're more easily
   // accessible in global contexts.
-  game.boilerplate = {
-    BoilerplateActor,
-    BoilerplateItem,
+  game.iconrpg = {
+    IconRPGActor,
+    IconRPGItem,
     rollItemMacro
   };
 
   // Add custom constants for configuration.
-  CONFIG.BOILERPLATE = BOILERPLATE;
+  CONFIG.ICONRPG = ICONRPG;
 
   /**
    * Set an initiative formula for the system
@@ -35,14 +35,14 @@ Hooks.once('init', async function() {
   };
 
   // Define custom Document classes
-  CONFIG.Actor.documentClass = BoilerplateActor;
-  CONFIG.Item.documentClass = BoilerplateItem;
+  CONFIG.Actor.documentClass = IconRPGActor;
+  CONFIG.Item.documentClass = IconRPGItem;
 
   // Register sheet application classes
   Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("boilerplate", BoilerplateActorSheet, { makeDefault: true });
+  Actors.registerSheet("iconrpg", IconRPGActorSheet, { makeDefault: true });
   Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("boilerplate", BoilerplateItemSheet, { makeDefault: true });
+  Items.registerSheet("iconrpg", IconRPGItemSheet, { makeDefault: true });
 
   // Preload Handlebars templates.
   return preloadHandlebarsTemplates();
@@ -93,7 +93,7 @@ async function createItemMacro(data, slot) {
   const item = data.data;
 
   // Create the macro command
-  const command = `game.boilerplate.rollItemMacro("${item.name}");`;
+  const command = `game.iconrpg.rollItemMacro("${item.name}");`;
   let macro = game.macros.find(m => (m.name === item.name) && (m.command === command));
   if (!macro) {
     macro = await Macro.create({
@@ -101,7 +101,7 @@ async function createItemMacro(data, slot) {
       type: "script",
       img: item.img,
       command: command,
-      flags: { "boilerplate.itemMacro": true }
+      flags: { "iconrpg.itemMacro": true }
     });
   }
   game.user.assignHotbarMacro(macro, slot);
--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -1,23 +1,23 @@
-export const BOILERPLATE = {};
+export const ICONRPG = {};
 
 /**
  * The set of Ability Scores used within the sytem.
  * @type {Object}
  */
- BOILERPLATE.abilities = {
-  "str": "BOILERPLATE.AbilityStr",
-  "dex": "BOILERPLATE.AbilityDex",
-  "con": "BOILERPLATE.AbilityCon",
-  "int": "BOILERPLATE.AbilityInt",
-  "wis": "BOILERPLATE.AbilityWis",
-  "cha": "BOILERPLATE.AbilityCha"
+ ICONRPG.abilities = {
+  "str": "ICONRPG.AbilityStr",
+  "dex": "ICONRPG.AbilityDex",
+  "con": "ICONRPG.AbilityCon",
+  "int": "ICONRPG.AbilityInt",
+  "wis": "ICONRPG.AbilityWis",
+  "cha": "ICONRPG.AbilityCha"
 };
 
-BOILERPLATE.abilityAbbreviations = {
-  "str": "BOILERPLATE.AbilityStrAbbr",
-  "dex": "BOILERPLATE.AbilityDexAbbr",
-  "con": "BOILERPLATE.AbilityConAbbr",
-  "int": "BOILERPLATE.AbilityIntAbbr",
-  "wis": "BOILERPLATE.AbilityWisAbbr",
-  "cha": "BOILERPLATE.AbilityChaAbbr"
+ICONRPG.abilityAbbreviations = {
+  "str": "ICONRPG.AbilityStrAbbr",
+  "dex": "ICONRPG.AbilityDexAbbr",
+  "con": "ICONRPG.AbilityConAbbr",
+  "int": "ICONRPG.AbilityIntAbbr",
+  "wis": "ICONRPG.AbilityWisAbbr",
+  "cha": "ICONRPG.AbilityChaAbbr"
 };
